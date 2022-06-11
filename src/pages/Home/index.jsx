e4aef1988dffafcbf28bb9ccff4550db1b89fec9
pablo_kleim <pablo.190777@gmail.com>
2022-06-11 00:57:35 -0300

    ajuste impl

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,42 +5,46 @@ import { WordsBox } from '../../components/WordsBox';
 import { Keyboard } from '../../components/Keyboard';
 import PALAVRAS from '../../palavras';
 
-const indicePalavra = 3;
-var letrasPalavra = [];
-
 export function Home() {
-  return (
-    <div className={styles.homeWrapper}>
-      <Header />
-      <WordsBox />
-      <Keyboard />
-      
-    </div>
-  )
-}
 
-//<Keyboard onClick={verificaPalavra}/>
-function verificaPalavra(tentativa) {//, indicePalavra
+  const indicePalavra = 3;
+  var retornoPalavras = [];
+  var letrasPalavra = [];
+
+  function verificaPalavra(tentativa = 'TESTE') {//, indicePalavra
     carregaLetrasPalavra(indicePalavra);
     tentativa = tentativa.toUpperCase();
 
     var letrasTentativa = tentativa.split("");
 
-    return letrasTentativa.map(defineCor);
-}
+    retornoPalavras = letrasTentativa.map(defineCor);
 
-function defineCor(letraTentativa, indiceLetra) {
+    console.log(tentativa);
+    console.log(PALAVRAS[indicePalavra]);
+    console.log(retornoPalavras);
+  }
+
+  function defineCor(letraTentativa, indiceLetra) {
     if (letraTentativa == letrasPalavra[indiceLetra]) {
-        return 'V'
+      return 'V'
     }
 
     if (letrasPalavra.slice(indiceLetra + 1).some(it => it === letraTentativa)) {
-        return 'A'
+      return 'A'
     }
 
     return 'C'
-}
+  }
 
-function carregaLetrasPalavra(indicePalavra){
+  function carregaLetrasPalavra(indicePalavra) {
     letrasPalavra = PALAVRAS[indicePalavra].split("");
+  }
+
+  return (
+    <div className={styles.homeWrapper}>
+      <Header />
+      <WordsBox />
+      <Keyboard onClick={verificaPalavra} />
+    </div>
+  )
 }
